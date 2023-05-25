727b454ac9072994427a7faf9d811a03bb04e83c
roshanbabu-Hutech <roshan.babu@hutechsolutions.com>
2023-05-25 17:17:27 +0530

    25-05-2023
    added db object

---
+++ WebApplication5/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
